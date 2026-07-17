a313411c66107bef74497d1fb24c8379ecf738fc
Rafael Fraga <rafalfraga@gmail.com>
2026-07-16 23:49:29 -0300

    feat: configura socket de escuta do servidor

--- src/server.c
+++ src/server.c
@@ -20,7 +20,43 @@ int main(void) {
         fail("Erro ao criar o socket");
     }
 
-    printf("Socket do servidor criado para a porta %d.\n", SERVER_PORT);
+    int reuse_address = 1;
+
+    if (setsockopt(
+            server_fd,
+            SOL_SOCKET,
+            SO_REUSEADDR,
+            &reuse_address,
+            sizeof(reuse_address)
+        ) == -1) {
+        close(server_fd);
+        fail("Erro ao configurar o socket");
+    }
+
+    struct sockaddr_in server_address = {0};
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    server_address.sin_port = htons(SERVER_PORT);
+
+    if (bind(
+            server_fd,
+            (struct sockaddr *)&server_address,
+            sizeof(server_address)
+        ) == -1) {
+        close(server_fd);
+        fail("Erro ao associar o socket");
+    }
+
+    if (listen(server_fd, CONNECTION_BACKLOG) == -1) {
+        close(server_fd);
+        fail("Erro ao iniciar a escuta");
+    }
+
+    printf(
+        "Servidor configurado em 127.0.0.1:%d.\n",
+        SERVER_PORT
+    );
 
     close(server_fd);
 
